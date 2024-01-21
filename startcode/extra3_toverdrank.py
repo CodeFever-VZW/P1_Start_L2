1d3bd74b82a69c0dde70344ddce7ad0c17cfa647
TessaCodefever <157284138+TessaCodefever@users.noreply.github.com>
2024-01-21 12:39:52 +0100

    Update extra3_toverdrank.py

--- startcode/extra3_toverdrank.py
+++ startcode/extra3_toverdrank.py
@@ -1,10 +1,10 @@
-# Welkom bij de Magische Toverdrank Brouwerij! Vandaag ga je je eigen magische toverdrank maken. Volg de onderstaande stappen om je eigen toverdrank te brouwen:
+# Volg de onderstaande stappen om je eigen toverdrank te brouwen:
 #
-# - Schrijf een functie genaamd mix_toverdrank die drie bestanddelen als parameters accepteert en een zin retourneert
+# - Schrijf een functie mix_toverdrank die 3 bestanddelen als parameters accepteert en een zin retourneert
 #   die aangeeft dat je een toverdrank hebt gemaakt met deze bestanddelen.
 #
-# - Schrijf een functie genaamd brouw_toverdrank die de gebruiker begroet en vervolgens naar drie bestanddelen vraagt.
+# - Schrijf een functie brouw_toverdrank die de gebruiker begroet en vervolgens naar 3 bestanddelen vraagt.
 #   Zeg nu aan de gebruiker dat je aan de slag gaat.
 #   Roep daarna de mix_toverdrank-functie aan om het resultaat te krijgen. Toon dit aan de gebruiker.
 #
-# - Voer de brouw_toverdrank functie uit om je eigen magische toverdrank te maken!
+# - Voer de brouw_toverdrank functie uit
