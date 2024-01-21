b58df72114b001a845b6128123d5e60889c52e4e
TessaCodefever <157284138+TessaCodefever@users.noreply.github.com>
2024-01-21 12:35:00 +0100

    Update b_papagaai.py

--- startcode/b_papagaai.py
+++ startcode/b_papagaai.py
@@ -1,11 +1,11 @@
+# Je hebt een functie genaamd verdubbel_getal gekregen die een getal verdubbelt.
+# Pas deze aan naar een functie genaamd papegaai, die 3 keer herhaalt wat je zegt.
+#
+# In plaats van een getal, moet de functie nu een string nemen als invoer
+# en deze string herhalen en teruggeven als resultaat.
+#
+# Bij invoer “Hoi” is de verwachte uitkomst “HoiHoiHoi”.
+
 def verdubbel_getal(getal):
     return getal * 2
 
-# Je hebt een functie genaamd verdubbel_getal gekregen die een getal verdubbelt.
-#
-# Kan jij deze functie nu aanpassen naar een functie genaamd papegaai, die als een papegaai 3 keer herhaalt wat je zegt?
-#
-# In plaats van een getal, moet de functie nu een string nemen als invoer en deze string herhalen en teruggeven als
-# resultaat.
-#
-# Bij invoer “Hoi” is de verwachte uitkomst “HoiHoiHoi”.
